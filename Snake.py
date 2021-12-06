f4aaf3a743eb68e85ff11bef8cc4211567ae08d6
Carlitosh636 <carloshervas63@hotmail.com>
2021-12-06 18:11:55 +0100

    Snake Green consumable

--- Snake.py
+++ Snake.py
@@ -2,111 +2,77 @@ import sys, random, pygame as pg
 
 pg.init()
 
-"""TO DO: Consumable que aumenta la puntuación de los azules, tener varios azules (opcional) y sonidos para la serpiente y consumables"""
-class Entity():
-    def __init__(self, rect) -> None:
-        self.rect = rect
-
-    def move_entity(self, screen, position, color):
-        self.rect = pg.Rect(position[0], position[1], 25, 25)
-        pg.draw.rect(screen, color, self.rect)
-
-
-class Consumable(Entity):
-    def __init__(self, rect, points):
-        self.rect = rect
-        self.points = points
-    def consumableEffect(self,list,body):
-        newPart = SnakePart(pg.Rect(body.rect.left, body.rect.top, 25, 25))
-        list.append(newPart)
-
-
-class SnakePart(Entity):
-    def __init__(self, rect):
-        super().__init__(rect)
-
-
-class Wall(Entity):
-    def __init__(self, rect):
-        super().__init__(rect)
-
-def calculate_pos_consum():
-    x = random.randint(-15, 15) * 25
-    y = random.randint(-15, 15) * 25
-    """se sale algunas veces y por consecuencia aparece demasiado en el borde. Probar las tecnicas de try catch que vienen en las diapos de clase a ver si es mejor."""
-    if x < 0:
-        x = 25
-    if x > 720:
-        x = 720
-    if y < 0:
-        y = 25
-    if y > 720:
-        y = 720
-    return [x, y]
-
 RED = (255, 0, 0)
 GREY = (128, 128, 128)
 BLUE = (0, 0, 255)
-screen = pg.display.set_mode((800, 800))
-import sys, random, pygame as pg
-
-pg.init()
-
-"""TO DO: Consumable que aumenta la puntuación de los azules, tener varios azules (opcional) y sonidos para la serpiente y consumables"""
-
-
-class Entity():
-    def __init__(self, rect) -> None:
-        self.rect = rect
-
-    def move_entity(self, screen, position, color):
-        self.rect = pg.Rect(position[0], position[1], 25, 25)
-        pg.draw.rect(screen, color, self.rect)
-
-
-class Consumable(Entity):
-    def __init__(self, rect, points):
-        self.rect = rect
-        self.points = points
-
-    def consumableEffect(self, list, body):
-        newPart = SnakePart(pg.Rect(body.rect.left, body.rect.top, 25, 25))
-        list.append(newPart)
-
-
-class SnakePart(Entity):
-    def __init__(self, rect):
-        super().__init__(rect)
-
-
-class Wall(Entity):
-    def __init__(self, rect):
-        super().__init__(rect)
-
-
-def calculate_pos_consum():
-    x = random.randint(-15, 15) * 25
-    y = random.randint(-15, 15) * 25
-    """se sale algunas veces y por consecuencia aparece demasiado en el borde. Probar las tecnicas de try catch que vienen en las diapos de clase a ver si es mejor."""
-    if x < 0:
-        x = 25
-    if x > 720:
-        x = 720
-    if y < 0:
-        y = 25
-    if y > 720:
-        y = 720
-    return [x, y]
-
-
-RED = (255, 0, 0)
-GREY = (128, 128, 128)
-BLUE = (0, 0, 255)
+GREEN = (0,255,0)
 screen = pg.display.set_mode((745, 745)) #(anchura,altura)
 position = [200, 50]
 amount = 5
 movementDict = {"w": (0, -5), "a": (-5, 0), "s": (0, 5), "d": (5, 0)}
 run = True
+valueMult = 1
+
+"""TO DO: Consumable que aumenta la puntuación de los azules y sonidos para la serpiente y consumables"""
+class Entity():
+    def __init__(self, rect) -> None:
+        self.rect = rect
+
+    def move_entity(self, screen, position, color):
+        self.rect = pg.Rect(position[0], position[1], 25, 25)
+        pg.draw.rect(screen, color, self.rect)
+    
+    def touchEffect(self):
+        raise NotImplementedError("Not implemented")
+
+
+class BlueConsumable(Entity):
+    def __init__(self, rect, color):
+        super().__init__(rect)
+        self.color=color
+    def touchEffect(self,list,body):
+        global valueMult
+        for i in range(0,valueMult):
+            newPart = SnakePart(pg.Rect(body.rect.left, body.rect.top, 25, 25))
+            list.append(newPart)
+
+class GreenConsumable(Entity):
+    def __init__(self, rect,color):
+        super().__init__(rect)
+        self.color=color
+    def touchEffect(self,list,body): #estos atributos no se usan. Buscar una manera de refactorizar touchEffect para que acepte una cantidad variable de atributos.
+        global valueMult
+        valueMult+=1
+
+class SnakePart(Entity):
+    def __init__(self, rect):
+        super().__init__(rect)
+    def touchEffect(self):
+        """meter aqui lo de si se toca, no moverse"""
+        return super().touchEffect()
+
+class Wall(Entity):
+    def __init__(self, rect):
+        super().__init__(rect)
+    def touchEffect(self):
+        """meter aqui lo de si se toca, no moverse"""
+        return super().touchEffect()
+
+def calculate_pos_consum():
+    x = random.randint(-15, 15) * 25
+    y = random.randint(-15, 15) * 25
+    """se sale algunas veces y por consecuencia aparece demasiado en el borde. Probar las tecnicas de try catch que vienen en las diapos de clase a ver si es mejor."""
+    if x < 0:
+        x = 25
+    if x > 720:
+        x = 720
+    if y < 0:
+        y = 25
+    if y > 720:
+        y = 720
+    return [x, y]
+
+
 '''posicion x, posicion y,anchura, altura'''
 head = SnakePart(pg.Rect(position[0], position[1], 25, 25))
 body = SnakePart(pg.Rect(position[0] - 25, position[1], 25, 25))
@@ -119,10 +85,13 @@ wall2 = Wall(pg.Rect(0, 0, 25, 720))
 wall3 = Wall(pg.Rect(0, 700, 720, 25))
 wall4 = Wall(pg.Rect(720, 0, 25, 720))
 walls = [wall1, wall2, wall3, wall4]
+
+
 consum1Position = [225, 100]
-consum1 = Consumable(pg.Rect(consum1Position[0], consum1Position[1], 25, 25), 1)
-consum2 = Consumable(pg.Rect(consum1Position[0] + 120, consum1Position[1] + 60, 25, 25), 1)
-consumables = [consum1, consum2]
+consum1 = BlueConsumable(pg.Rect(consum1Position[0], consum1Position[1], 25, 25), BLUE)
+consum2 = BlueConsumable(pg.Rect(consum1Position[0] + 125, consum1Position[1] + 60, 25, 25), BLUE)
+consum3 = GreenConsumable(pg.Rect(consum1Position[0] + 150, consum1Position[1] + 60, 25, 25),GREEN)
+consumables = [consum1, consum2,consum3]
 """Añadir un consumable verde que suba el valor del consumable azul por 2"""
 
 prevPositions = []
@@ -164,16 +133,10 @@ while run:
                     """Si toca un consumable, añade 1 pieza a la serpiente. Cambiarlo para que la cantidad de piezas añadidas dependa del atributo points de la clase Consumable"""
                     collidedConsum = testRect.collidelist(consumables)
                     if collidedConsum != -1:
-                        consumables[collidedConsum].consumableEffect(snake, body)
+                        consumables[collidedConsum].touchEffect(snake, body)
                         consum1Position = calculate_pos_consum()
-                        consumables[collidedConsum].rect.left = consum1Position[
-                            0]  # cambia la pos del consumable tomado
+                        consumables[collidedConsum].rect.left = consum1Position[0]  # cambia la pos del consumable tomado
                         consumables[collidedConsum].rect.top = consum1Position[1]
-
-                    if testRect.colliderect(consum1.rect):
-                        newPart = SnakePart(pg.Rect(body.rect.left, body.rect.top, 25, 25))
-                        snake.append(newPart)
-                        consum1Position = calculate_pos_consum()
 
                     """Mirar que no choque consigo mismo. Collidelist devuelve el index del objeto tocado."""
                     if testRect.collidelist(snake) != -1:
@@ -194,7 +157,7 @@ while run:
     screen.fill((0, 0, 0))
     count = 0
     for consum in consumables:
-        consum.move_entity(screen, consum.rect, BLUE)
+        consum.move_entity(screen, consum.rect, consum.color)
 
     if canMove:
         for part in snake:
